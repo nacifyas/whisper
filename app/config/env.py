81613b473e12386be46b40c22ef74620dc0d84ea
nacifyas <nacifyas@gmail.com>
2023-06-20 23:50:47 +0200

    Fixed environment validator bug

--- app/config/env.py
+++ app/config/env.py
@@ -1,22 +1,17 @@
 from pydantic import BaseSettings, validator
+import os
 import whisper
 
 
 class Settings(BaseSettings):
-    model: str
     in_memory: bool = True
     dev_mode: bool = False
     port: int = 9900
     delete_on_finish: bool = True
     audio_dir: str = "audio"
     model_dir: str | None = None
-
-    @validator('model')
-    def model_validity(cls, v: str):
-        models = whisper.available_models()
-        if v not in models:
-            raise ValueError(f'Model not available. Possible options: {models}')
-        return v
+    model: str
+                
 
     class Config:
         env_file = ".env"
